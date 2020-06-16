2f6b5d6dea850eae005edf5b30bdda40562d55a7
Gilbert-hu <66233017+Gilbert-hu@users.noreply.github.com>
2020-06-16 19:56:46 +0200

    Update roboczy.py

--- roboczy.py
+++ roboczy.py
@@ -11,14 +11,12 @@ q = Queue()
 #pobieranie z kolejki
 ip = ''
 def send():
-    #print(type(ip), ip)
     EMAIL_ADDRESS = 'hiltkom@gmail.com'#os.environ.get('EMAIL_USER')
     PASS = 'SoundEpic69'#os.environ.get('EMAIL_PASS')
 
     message = EmailMessage()
     message['Subject'] = 'Awaria łącza'
     message['From'] = EMAIL_ADDRESS
-    #message['To'] = 'Hubert.Ligocki@jeronimo-martins.com'
     message['To'] = 'hubert.ligocki@gmail.com'
 
     message.set_content('Wiadomosc testowa. Proszę ją zignorowac ')
@@ -32,28 +30,21 @@ def main():
 
     print('wykonuje glowne funkcje')
     ip = q.get()
-    #print('1')
+
     response_list = ping(ip, size = 1, count = 2, timeout = 1)
-    #print('2')
-    #print(response_list.rtt_avg_ms)
-    if response_list.rtt_avg_ms > 4:
-        print(response_list)
-        #print(ip)
-        #send()
-        #print('adres NIE odpowiada')
+
+    if response_list.rtt_avg_ms > 5:
+
+        print('adres NIE odpowiada')
     else:
         print('adres odpowiada')
     q.task_done()
 
-    #print('koncze wykonywanie glownej funkcji')
-
 for x in range(3):
     print('check')
     t = threading.Thread(target=main)
-    #t.daemon = True
     t.start()
     print(t)
 
 for ip in address_list:
-    #print(ip)
     ip = q.put(ip)
